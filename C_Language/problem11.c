463973423db385ccb39ffe552fbfa2a3e8a4a29d
George Mavroidis <georgeymavroidis@gmail.com>
2019-11-26 10:47:31 -0500

    Problem 11

---
+++ C_Language/problem11.c
@@ -0,0 +1,116 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int **makeGrid();
+int largest(int arr[], int n);
+
+int main(void){
+
+
+  int **grid = makeGrid();
+
+  long max = 0;
+  int length = 20;
+  int step = 4;
+
+  for (int y = 0; y < length; y++) {
+    for (int x = 0; x < length; x++) {
+      int horizontal = 1, vertical = 1, diag_up = 1, diag_down= 1;
+      //Check Horizontals, Verticals, Diagonal-Up, Diagonal-Down
+      for (int i = 0; i < step; i++) {
+            if((x + step) <= length){
+                //Check horizontal
+                horizontal *= grid[y][i+x];
+                // printf("y: %d x: %d %d %d\n", y, i+x, grid[y][x+i], horizontal);
+            }
+            if((y + step) <= length){
+                //Check vertical
+                vertical *= grid[y+i][x];
+                // printf("y: %d x: %d %d %d\n", y+i, x, grid[y+i][x], vertical);
+            }
+            if(((y + step) <= length) && ((x + step) <= length)){
+                //check diag-down
+                diag_down *= grid[y+i][x+i];
+                // printf("y: %d x: %d %d %d\n", y+i, x+i, grid[y+i][x+i], diag_down);
+            }
+            if(((y + step) <= length) && ((x - step) > 0)){
+                //check diag-up
+                diag_up *= grid[y+i][x-i];
+                // printf("y: %d x: %d %d %d\n", y+i, x-i, grid[y+i][x-i], diag_up);
+            }
+
+
+
+      }
+
+      // int max_array[5];
+      int max_array[5] = {horizontal, vertical, diag_up, diag_down, max};
+      // int n = sizeof(max_array)/sizeof(max_array[0]); 
+      max = largest(max_array, 5);
+
+            // p1 *= grid[y][x+1];
+            // p2 *= grid[y+1][x];
+            // p3 *= grid[y+1][x+1];
+            // p4 *= grid[y+1][x-1];
+    }
+  }
+
+      printf("%ld\n", max);
+  // return max;
+
+  return 0;
+
+}
+int **makeGrid(){
+    char grid_string[] = "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08 49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00 81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65 52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91 22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80 24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50 32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70 67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21 24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72 21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95 78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92 16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57 86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58 19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40 04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66 88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69 04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36 20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16 20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54 01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48";
+    int init_size = strlen(grid_string);
+    
+    char delim[] = " ";
+    char *grid_delim= strtok(grid_string, delim);
+
+    int grid_int[init_size];
+    int count = 0;
+
+    while(grid_delim != NULL){
+
+        char frankenstr[3];
+        frankenstr[0] = grid_delim[0];
+        frankenstr[1] = grid_delim[1];
+        frankenstr[2] = '\0';
+        int num = atoi(frankenstr);
+
+        grid_int[count] = num;
+
+        count++;
+        grid_delim = strtok(NULL, delim);
+
+    }
+
+    int length = 20;
+    int ** grid = (int**)malloc(length * length * sizeof(int*));
+
+    count = 0;
+    for (int arr = 0; arr < length; arr++){
+        grid[arr] = malloc(length * sizeof(int)); 
+        for (int idx = 0; idx < length; idx++){
+            grid[arr][idx] = grid_int[count];
+            count++;
+        }
+    }
+    return (int **)grid;
+}
+int largest(int arr[], int n) { 
+    int i; 
+    // Initialize maximum element 
+    int max = arr[0]; 
+    // Traverse array elements  
+    // from second and compare 
+    // every element with current max  
+    for (i = 1; i < n; i++) 
+        if (arr[i] > max) 
+            max = arr[i]; 
+  
+    return max; 
+} 
+
